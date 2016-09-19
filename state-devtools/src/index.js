c68a42ff7ef665628ac26e4fb71d2384b428bbcf
Renán Romero <renanromero@gmail.com>
2016-09-19 16:08:23 -0500

    feat(state) demo state

---
+++ state-devtools/src/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Examples extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {count: 0, sum: 3, a:1, b:2}
+  }
+  render() {
+    return (
+      <div>
+        <h1>React</h1>
+        Clicked: <span>{this.state.count}</span> times 
+        <button>Decrease</button>
+        <p/>
+        <input value={this.state.a}/> + <input value={this.state.b}/>
+        = <span>{this.state.sum}</span>
+        <p/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Examples />,
+  document.getElementById('root')
+)
